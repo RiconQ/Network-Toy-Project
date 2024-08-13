d7c3601593b4993d27b2ac0bc6f6223ede20abbd
이준형 <dkflfdj@gmail.com>
2024-08-13 17:45:26 +0900

    [Update] 파티클 동기화

--- Assets/3.Script/LEE JUNHYEONG/VFXscript/ParticleControl.cs
+++ Assets/3.Script/LEE JUNHYEONG/VFXscript/ParticleControl.cs
@@ -1,11 +1,12 @@
+/*using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem.Switch;
 using static ParticleControl;
+using Mirror;
 
 public enum ParticleName
 {
@@ -14,11 +15,11 @@ public enum ParticleName
     DIE =2
 }
 
-public class ParticleControl : MonoBehaviour
+public class ParticleControl : NetworkBehaviour
 {
     /*
      * 스킬을 누르면 enum 값으로 맞는 스킬을 배열에 index로 접근하여 활성 / 비활성
-     */
+     *//*
     [SerializeField]
     private ParticleSystem[] particleSystems; // 파티클
 
@@ -26,17 +27,12 @@ public class ParticleControl : MonoBehaviour
     private AudioClip[] audioClips;
     private AudioSource PlayerAudio;
 
-    private UnityAction OnParticle;
-
-    private void Start()
+    private static event Action<Vector3,Vector3> OnHit;
+    public override void OnStartAuthority()
     {
-        PlayerAudio = GetComponent<AudioSource>();
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            particleSystems[i] = transform.GetChild(i).GetComponent<ParticleSystem>();
-        }
+        Debug.Log("권한 부여 완료");
+        OnHit += HitEFF;
     }
-
 
     public void StartEFFByState(ParticleName state) // 상태에 따른 이펙트
     {
@@ -44,24 +40,44 @@ public class ParticleControl : MonoBehaviour
             particleSystems[(int)state].Play();
     }
 
+    [Command(requiresAuthority = false)]
+    private void CMDHandle(Vector3 hitPos, Vector3 hitDirection)
+    {
+        Debug.Log("커맨드");
+        RPCHandleHit(hitPos, hitDirection);
+    }
+
+    [ClientRpc]
+    private void RPCHandleHit(Vector3 hitPos, Vector3 hitDirection)
+    {
+        Debug.Log("RPC 핸들러");
+        OnHit?.Invoke(hitPos, hitDirection);
+    }
+
     private void HitEFF(Vector3 hitPos, Vector3 hitDirection) // 맞는 효과
     {
+        Debug.Log("이벤트 실행");
         particleSystems[(int)ParticleName.HIT].transform.position = hitPos;
 
         particleSystems[(int)ParticleName.HIT].transform.LookAt(
             hitDirection, Vector3.up);
 
+
+        particleSystems[(int)ParticleName.HIT].Stop();
         particleSystems[(int)ParticleName.HIT].Play();
+        particleSystems[(int)ParticleName.DIE].Stop();
         particleSystems[(int)ParticleName.DIE].Play();
     }
 
+    [Client]
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.name.Equals("Weapon"))
         {
-            Debug.Log("충돌 이펙트");
-            StartSoundByEFFState(ParticleName.DIE);
-            HitEFF(other.ClosestPoint(transform.position), other.ClosestPoint((2 * other.transform.position) - transform.position));
+            Debug.Log("충돌");
+            //StartSoundByEFFState(ParticleName.DIE);
+
+            CMDHandle(other.ClosestPoint(transform.position), other.ClosestPoint((2 * other.transform.position) - transform.position));
         }
     }
 
@@ -71,3 +87,4 @@ public class ParticleControl : MonoBehaviour
         PlayerAudio.Play();
     }
 }
+*/
