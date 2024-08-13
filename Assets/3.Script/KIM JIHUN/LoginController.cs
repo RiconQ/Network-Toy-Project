854fa4b2bb6edfb793d7382bd51dfea7e1ba6fdf
김지훈 <ricon4246@gmail.com>
2024-08-13 14:09:11 +0900

    [Upadate] 로그인 UI Update

--- Assets/3.Script/KIM JIHUN/LoginController.cs
+++ Assets/3.Script/KIM JIHUN/LoginController.cs
@@ -7,29 +7,48 @@ using DG.Tweening;
 
 public class LoginController : MonoBehaviour
 {
-    [Header("Text")]
-    [SerializeField] private TMP_InputField _userName;
-    [SerializeField] private TMP_InputField _password;
+    [Header("Common")]
+    [SerializeField] private TMP_Text _title;
+    [SerializeField] private Image _iconIMG;
+    [SerializeField] private Sprite[] _icons; 
+
+    [Header("Login Text")]
+    [SerializeField] private TMP_InputField _loginEmail;
+    [SerializeField] private TMP_InputField _loginPassword;
     [SerializeField] private TMP_Text _logText;
 
     [Header("Button")]
+    [SerializeField] private Button _closeBTN;
     [SerializeField] private Button _loginBTN;
     [SerializeField] private Button _registerBTN;
-    [SerializeField] private Button _closeBTN;
+
+    [Header("Register Text")]
+    [SerializeField] private TMP_InputField _registerEmail;
+    [SerializeField] private TMP_InputField _registerUserName;
+    [SerializeField] private TMP_InputField _registerPassword;
 
     [Header("Obj")]
+    [SerializeField] private GameObject _loginContainer;
+    [SerializeField] private GameObject _registerContainer;
     [SerializeField] private GameObject _popupObj;
     [SerializeField] private GameObject _logigBtnObj;
+
+    private bool _isRegister = false;
 
     /// <summary>
     /// When Click Login Button In MainMenu
     /// </summary>
     public void Open()
     {
+        _isRegister = false;
         _loginBTN.onClick.AddListener(Login);
         _registerBTN.onClick.AddListener(Register);
         _closeBTN.onClick.AddListener(Close);
-        
+
+        _title.text = "로그인";
+        _iconIMG.sprite = _icons[0];
+        _registerContainer.SetActive(false);
+        _loginContainer.SetActive(true);
     }
 
     /// <summary>
@@ -37,8 +56,9 @@ public class LoginController : MonoBehaviour
     /// </summary>
     private void Close()
     {
-        _userName.text = string.Empty;
-        _password.text = string.Empty;
+        _isRegister = false;
+        _loginEmail.text = string.Empty;
+        _loginPassword.text = string.Empty;
 
         _loginBTN.onClick.RemoveAllListeners();
         _registerBTN.onClick.RemoveAllListeners();
@@ -59,14 +79,36 @@ public class LoginController : MonoBehaviour
     /// </summary>
     private void Login()
     {
+        //회원가입 창이라면 Login창으로 전환
+        if (_isRegister)
+        {
+            _registerContainer.SetActive(false);
+            _loginContainer.SetActive(true);
 
-        // 로그인에 성공했다면 메인메뉴 로그인 버튼 비활성화
-        /*
-         * if(Login Success)
-         * {
-         *      _logigBtnObj.SetActive(false);
-         * }
-         */
+            _title.text = "로그인";
+            _iconIMG.sprite = _icons[0];
+
+            _isRegister = false;
+        }
+        //로그인 창이라면 로그인 로직, 로그인 성공시 로그인 창 비활성화 후 메인메뉴 로그인 버튼 비활성화
+        else
+        {
+            //로그인 로직
+
+
+            //로그인 성공시 로그인 창 비활성화 후 메인메뉴 로그인 버튼 비활성화
+            //if(로그인 성공)
+            Close();
+            _logigBtnObj.SetActive(false);
+
+            /*
+            else
+            {
+                //로그인 실패 Log 출력
+                _logText.text = "로그인 실패";
+            }
+            */
+        }
     }
 
     /// <summary>
@@ -74,7 +116,30 @@ public class LoginController : MonoBehaviour
     /// </summary>
     private void Register()
     {
+        // 회원가입 창이라면 회원가입 로직, 이후 로그인 창으로
+        if (_isRegister)
+        {
+            //회원가입 로직
 
+            //회원가입 성공시 로그인 창으로 전환
+            _registerContainer.SetActive(false);
+            _loginContainer.SetActive(true);
+
+            _title.text = "로그인";
+            _iconIMG.sprite = _icons[0];
+            _isRegister = false;
+        }
+        // 로그인 창이라면 회원가입 모드로 전환
+        else
+        {
+            _registerContainer.SetActive(true);
+            _loginContainer.SetActive(false);
+
+            _title.text = "회원가입";
+            _iconIMG.sprite = _icons[1];
+
+            _isRegister = true;
+        }
     }
 
 }
