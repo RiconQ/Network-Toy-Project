f98570b2d0af2ac2fd63d87546c4b9a6930184d6
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2024-08-16 18:30:12 +0900

    [Fix] host & client scirpts
    호스트&클라이언트 로직 동기화 진행중

--- Assets/3.Script/HONG SEONGGYEON/CameraTransform.cs
+++ Assets/3.Script/HONG SEONGGYEON/CameraTransform.cs
@@ -1,14 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class CameraTransform : MonoBehaviour
+public class CameraTransform : NetworkBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    private Camera _camera;
 
-    private void Update()
+    private void Start()
     {
-        gameObject.transform.position = target.position + offset;
+        if (isLocalPlayer)  // authority가 아니라 hasAuthority로 수정
+        {
+            // 메인 카메라를 현재 오브젝트의 자식으로 설정
+            _camera = Camera.main;
+            if (_camera != null)
+            {
+                // 카메라를 이 스크립트가 붙은 오브젝트의 자식으로 설정
+                _camera.transform.SetParent(transform);
+                _camera.transform.localPosition = offset;
+                _camera.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isLocalPlayer && _camera != null)
+        {
+            // 타겟 위치를 기준으로 카메라 위치 갱신
+            _camera.transform.position = target.position + offset;
+        }
     }
 }
